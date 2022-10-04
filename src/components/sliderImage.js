baef0dc9ccdd5d8f17fe51b920ceee98192e828e
Alexey Kachaykin <108185407+AlexeyKachaykin@users.noreply.github.com>
2022-10-04 15:55:20 +0300

    finish

---
+++ src/components/sliderImage.js
@@ -0,0 +1,41 @@
+import First from "../assets/First.jpg";
+import Second from "../assets/Second.jpg";
+import Third from "../assets/Third.jpg";
+
+export default [
+  {
+    title: "First Slide",
+    description: "This is the first slider Image of our carousel",
+    urls: First,
+  },
+  {
+    title: "Second Slide",
+    description: "This is the second slider Image of our carousel",
+    urls: Second,
+  },
+  {
+    title: "Third Slide",
+    description: "This is the Third slider Image of our carousel",
+    urls: Third,
+  },
+  {
+    title: "Fourth Slide",
+    description: "This is the Fourth slider Image of our carousel",
+    urls: "https://storge.pic2.me/c/1360x800/476/630950ef4d3c06.54022677.jpg",
+  },
+  {
+    title: "Fifth Slide",
+    description: "This is the Fifth slider Image of our carousel",
+    urls: "https://storge.pic2.me/c/1360x800/677/54a9985d97fed.jpg",
+  },
+  {
+    title: "Sixth Slide",
+    description: "This is the Sixth slider Image of our carousel",
+    urls: "https://storge.pic2.me/c/1360x800/838/54a997694379b.jpg",
+  },
+  {
+    title: "Seventh Slide",
+    description: "This is the Seventh slider Image of our carousel",
+    urls: "https://storge.pic2.me/c/1360x800/169/6247981eb27490.53424743.jpg",
+  },
+];
